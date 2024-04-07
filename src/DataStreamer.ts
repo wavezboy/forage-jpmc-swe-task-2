81f86c7f80f779348510ba2032323940f3b6e343
wavezboy <sanusifatah16@gmail.com>
2024-04-07 22:20:21 +0100

    .

--- src/DataStreamer.ts
+++ src/DataStreamer.ts
@@ -1,40 +1,54 @@
 export interface Order {
-  price: Number,
-  size: Number,
+  price: number;
+  size: number;
 }
-/**
- * The datafeed server returns an array of ServerRespond with 2 stocks.
- * We do not have to manipulate the ServerRespond for the purpose of this task.
- */
+
 export interface ServerRespond {
-  stock: string,
-  top_bid: Order,
-  top_ask: Order,
-  timestamp: Date,
+  stock: string;
+  top_bid: Order;
+  top_ask: Order;
+  timestamp: Date;
 }
 
 class DataStreamer {
-  // The url where datafeed server is listening
-  static API_URL: string = 'http://localhost:8080/query?id=1';
+  static API_URL = "http://localhost:8080/query?id=1";
 
   /**
-   * Send request to the datafeed server and executes callback function on success
-   * @param callback callback function that takes JSON object as its argument
+   * Send a request to the datafeed server and execute the callback function on success.
+   * @param callback A callback function that takes an array of ServerRespond objects as its argument.
    */
-  static getData(callback: (data: ServerRespond[]) => void): void {
-    const request = new XMLHttpRequest();
-    request.open('GET', DataStreamer.API_URL, false);
+  // static async getData(
+  //   callback: (data: ServerRespond[]) => void
+  // ): Promise<void> {
+  //   try {
+  //     const response = await fetch(DataStreamer.API_URL);
+  //     if (!response.ok) {
+  //       throw new Error("Request failed");
+  //     }
+  //     const data = await response.json();
+  //     callback(data);
+  //   } catch (error) {
+  //     console.error("Error fetching data:", error);
+  //     alert("Request failed");
+  //   }
+  // }
 
-    request.onload = () => {
-      if (request.status === 200) {
-        callback(JSON.parse(request.responseText));
-      } else {
-        alert ('Request failed');
+  static getData = async (
+    callback: (data: ServerRespond[]) => void
+  ): Promise<void> => {
+    try {
+      const response = await fetch(DataStreamer.API_URL);
+      if (!response.ok) {
+        throw new Error("Request Failed");
       }
-    }
 
-    request.send();
-  }
+      const data = await response.json();
+      callback(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      alert("Request failed");
+    }
+  };
 }
 
 export default DataStreamer;
